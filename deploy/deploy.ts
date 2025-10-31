1315f1c5da349ffce23e02a91cd58e93d132399a
hiroshi-tanri <mlqfuyrw741@outlook.com>
2025-10-31 15:57:16 +0800

    deploy

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -5,13 +5,25 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
-  const deployedFHECounter = await deploy("FHECounter", {
+  const zcoinDeployment = await deploy("ERC7984Zcoin", {
     from: deployer,
     log: true,
   });
 
-  console.log(`FHECounter contract: `, deployedFHECounter.address);
+  const stakingDeployment = await deploy("EncryptedStaking", {
+    from: deployer,
+    args: [zcoinDeployment.address],
+    log: true,
+  });
+
+  const deployerSigner = await hre.ethers.getSigner(deployer);
+  const zcoinContract = await hre.ethers.getContractAt("ERC7984Zcoin", zcoinDeployment.address, deployerSigner);
+  const tx = await zcoinContract.setStakingContract(stakingDeployment.address);
+  await tx.wait();
+
+  console.log(`ERC7984Zcoin deployed at ${zcoinDeployment.address}`);
+  console.log(`EncryptedStaking deployed at ${stakingDeployment.address}`);
 };
 export default func;
-func.id = "deploy_fheCounter"; // id required to prevent reexecution
-func.tags = ["FHECounter"];
+func.id = "deploy_encrypted_staking"; // id required to prevent reexecution
+func.tags = ["EncryptedStaking", "ERC7984Zcoin"];
